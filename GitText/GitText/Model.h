14aa0a23ee8c657b4c6bbabb55838b067c1e6be7
lanou3g <0223fuzzzzzz@gmail.com>
2016-05-11 20:25:40 +0800

    添加一个model

---
+++ GitText/GitText/Model.h
@@ -0,0 +1,13 @@
+//
+//  Model.h
+//  GitText
+//
+//  Created by lanou3g on 16/5/11.
+//  Copyright © 2016年 芝芝. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Model : NSObject
+
+@end
---
+++ GitText/GitText/Model.m
@@ -0,0 +1,13 @@
+//
+//  Model.m
+//  GitText
+//
+//  Created by lanou3g on 16/5/11.
+//  Copyright © 2016年 芝芝. All rights reserved.
+//
+
+#import "Model.h"
+
+@implementation Model
+
+@end
