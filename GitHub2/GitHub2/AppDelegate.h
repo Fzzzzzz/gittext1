4daf1910c261d68e428653c222bd717dd3490500
lanou3g <0223fuzzzzzz@gmail.com>
2016-05-11 20:03:26 +0800

    这是一个新的文件

---
+++ GitHub2/GitHub2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHub2
+//
+//  Created by lanou3g on 16/5/11.
+//  Copyright © 2016年 芝芝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
